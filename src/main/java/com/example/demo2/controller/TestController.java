61defb2a683c5f07e47357170f966eae34fe47f1
nakamurasyunsuke <nakamura@feliseed.co.jp>
2022-02-25 01:24:37 +0900

    testcommit

---
+++ src/main/java/com/example/demo2/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.example.demo2.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/index")
+    public String index(Model model){
+        model.addAttribute("tag","Spr Boot + Thymeleaf入門");
+        return "index";
+    }
+
+    @RequestMapping("/input")
+    public String input(String testInput, Model model){
+        model.addAttribute("tag", "Spring Boot + Thymeleaf入門");
+        testInput = testInput + testInput;
+        model.addAttribute("testInput", testInput);
+        return "index";
+    }
+
+
+}
