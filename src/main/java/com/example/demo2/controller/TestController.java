07322b059542eb8d735caa9d0979496650033514
nakamurasyunsuke <nakamura@feliseed.co.jp>
2022-02-26 23:20:11 +0900

    [初等整数論]素因数分解のプログラムの実装中

--- src/main/java/com/example/demo2/controller/TestController.java
+++ src/main/java/com/example/demo2/controller/TestController.java
@@ -4,7 +4,7 @@ package com.example.demo2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.example.service.IntegerServiceImpl;
 
 
 @Controller
@@ -22,6 +22,18 @@ public class TestController {
         testInput = testInput + testInput;
         model.addAttribute("testInput", testInput);
         return "index";
+    }
+
+    @RequestMapping("/integer")
+    public String integer(){
+        return "integer";
+    }
+
+    @RequestMapping("/primeFactorization")
+    public int[] primeFactorization(int integer, Model model){
+        IntegerServiceImpl impl = new IntegerServiceImpl();
+        int [] primeFactors = impl.getPrimeFactorsOf(integer);
+        return primeFactors;
     }
 
 }
---
+++ src/main/java/com/example/service/IntegerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IntegerServiceImpl implements IntegerService {
+    
+    @Override
+    public int[] getPrimeFactorsOf(int integer) {
+        int[] ints = new int[3];
+        return ints;
+    }
+}
