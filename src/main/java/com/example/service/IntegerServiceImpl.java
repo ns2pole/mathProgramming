063684adf4cca66daf9ae12ac1d0b309b8de0f43
nakamurasyunsuke <nakamura@feliseed.co.jp>
2022-02-27 00:56:27 +0900

    jqueryの追加と実験コードの追加

--- src/main/java/com/example/service/IntegerServiceImpl.java
+++ src/main/java/com/example/service/IntegerServiceImpl.java
@@ -8,6 +8,9 @@ public class IntegerServiceImpl implements IntegerService {
     @Override
     public int[] getPrimeFactorsOf(int integer) {
         int[] ints = new int[3];
+        ints[0] = 4;
+        ints[1] = 5;
+        ints[2] = 6;
         return ints;
     }
 }
